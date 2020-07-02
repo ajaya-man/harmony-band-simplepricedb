cd403654c85e21debcc0d92b501886e0d7d7b3cc
Saroj Shrestha <sarojsht377@gmail.com>
2020-07-02 21:37:20 +0545

    Initial commit

--- migrations/2_simple_price_database.js
+++ migrations/2_simple_price_database.js
@@ -2,5 +2,5 @@ const SimplePriceDB = artifacts.require("SimplePriceDB");
 
 module.exports = function(deployer) {
   // Use deployer to state migration tasks.
-  deployer.deploy(SimplePriceDB, "0x96Ab34Bda8414f329604a151d89982B19C680c17" );
+  deployer.deploy(SimplePriceDB, "0x0671E9734bDE19eb744Df3f80FAba52a0994b1F4" );
 };
